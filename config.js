acc996e09ffd7e6b74f3bf8f8dbb9bee76052c0c
f4botz <107820254+f4botz@users.noreply.github.com>
2022-06-21 13:02:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By faixd.
+   * Contact Me on wa.me/6289518901841
+   * Follow gw
 */
 
 const fs = require('fs')
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6289518901841','6289518901841','6289518901841']
+global.premium = ['629518901841']
+global.packname = 'F4 BOTZ'
 global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.sessionName = 'Faixd'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
